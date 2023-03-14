93dd3dd7352a0290bd5f6c0b0114387f6db6785e
Bruno Arruda <arrudabruno10@gmail.com>
2023-03-14 00:01:28 -0300

    feat(ec2/challenge): aprimora script de instalação do minikube

--- infrastructure/lab/ec2/challenge/main.tf
+++ infrastructure/lab/ec2/challenge/main.tf
@@ -7,11 +7,36 @@ terraform {
   }
 }
 
+locals {
+  user_data = <<EOF
+#!/bin/bash
+echo "installing podman"
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl --silent -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | sudo apt-key add -
+apt-get -qq update
+apt-get -qq -y install podman
+
+echo "installing minikube"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64s
+
+su ubuntu
+PUBLIC_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+minikube start --driver=podman --container-runtime=cri-o --listen-address=0.0.0.0 --embed-certs
+
+minikube kubectl -- create namespace argocd
+minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+EOF
+}
+
 module "ec2" {
     source = "../../../../module/ec2"
     name = var.name
     ami = var.ec2_ami
     instance_type = var.instance_type
+
+    user_data = local.user_data
 
     public_ip_address = var.public_ip_address
     subnet_id = var.subnet_id
